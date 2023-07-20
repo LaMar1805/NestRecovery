3f61f2dd7842355bd9eddd3c03ee72320815625b
DmitriyAgapov <prweb2012@gmail.com>
2023-07-20 23:38:00 +0500

    video fallback fix

--- components/Accordion/AccordionTitle.tsx
+++ components/Accordion/AccordionTitle.tsx
@@ -1,11 +1,12 @@
 
-import { EventHandler } from "react";
+import { EventHandler, useId } from "react";
 import { ArrowASvg, ArrowSvg } from "@/components/Icons";
 const ArrowCss = () =>   <span className={'arrow--l-r right'}><span/><span/><span/><span/><span/></span>
 
 export const AccordionTitle = ({title, action} : {title: string, action: EventHandler<any> }) => {
-	return <div className={"accordion__title"}>
-		<h3 onClick={action}>{title}<ArrowCss/></h3>
+	const id = useId()
+	return <div className={"accordion__title"} id={id}>
+		<h3 onClick={action}><a href={`#${id}`}> {title}<ArrowCss/></a></h3>
 	</div>;
 }
 export const AccordionText = ({text} : { text: string | TrustedHTML }) => {
