a757acefa7fff5a75f8d6282388b49e6bb606287
DmitriyAgapov <prweb2012@gmail.com>
2023-08-17 08:52:57 +0500

    ImageLoader orientaion + video HLS

--- components/VideoPlayer/CloudinaryVideoCustom.tsx
+++ components/VideoPlayer/CloudinaryVideoCustom.tsx
@@ -1,105 +1,46 @@
-'use client'
-import React, { useRef } from 'react'
-import {AdvancedVideo, lazyload } from '@cloudinary/react';
-import { Cloudinary, CloudinaryVideo, } from "@cloudinary/url-gen";
-// import { Simulate } from "react-dom/test-utils";
-// import { scale } from "@cloudinary/url-gen/actions/resize";
-import { format, quality } from "@cloudinary/url-gen/actions/delivery";
-import { auto } from "@cloudinary/url-gen/qualifiers/quality";
-import { auto as autoFormat } from "@cloudinary/url-gen/qualifiers/format";
-import { auto as autoQuality } from "@cloudinary/url-gen/qualifiers/quality";
-import { auto as autoVideo } from "@cloudinary/url-gen/qualifiers/videoCodec";
-import { name } from "@cloudinary/url-gen/actions/namedTransformation";
-// @ts-ignore
-// import  VideoPlayer  from "cloudinary-video-player";
-
-import { videoCodec } from "@cloudinary/url-gen/actions/transcode";
-
-// @ts-ignore
-const CloudinaryVideoCustom = () => {
-	// const cld = cloudinary.videoPlayer('example-player', {
-	// 	cloud_name: 'dpiuthi6q'
-	// });
-	// const player = cld.videoPlayer('player', {
-	// 	fluid: true,
-	// 	controls: false,
-	// 	muted: false,
-	// 	hideContextMenu: true,
-	// 	floatingWhenNotVisible: false,
-	// 	autoplayMode: 'on-scroll',
-	// 	autoplay: true
-	// });
-	//
-	// player.source('elephants', {
-	// 	sourceTypes: ['hls'],
-	// 	transformation: {
-	// 		streaming_profile: 'auto'
-	// 	}
-	// });
-	// Use the video with public ID, 'docs/walking_talking'.
-	// const myVideo = cld.video('docs/walking_talking');https://res.cloudinary.com/dpiuthi6q/video/upload/s--pnsDz6Xb--/f_auto:video,q_auto/v1/nest/1/xjpchft1uit3cf5gzhtt
-	const ref = useRef();
-	// const sources = [
-	// 	{
-	// 		type: 'mp4',
-	// 		codecs: ['vp8', 'vorbis'],
-	// 		transcode: videoCodec(autoVideo())
-	// 	},
-	// 	{
-	// 		type: 'webm',
-	// 		codecs: ['avc1.4D401E', 'mp4a.40.2'],
-	// 		videoCodec: videoCodec(autoVideo())
-	// 	}];
-	let myVideo = new CloudinaryVideo('nest/c5g3xpzt4ubjuukiuxv5', {
-		apiKey: '195655714938893',
-		apiSecret: 'ciavXR0Z0wCiXkhFRAeOAyDewYM',
-		cloudName: 'dpiuthi6q'
-	}, {
-		secure: true
-	}).namedTransformation(name("VIDSCREEN"))
-	// .delivery(format(autoFormat()))
-	.delivery(quality(autoQuality())
-
-	);
-	// const player = cld('video', {
-	// 	fluid: true,
-	// 	controls: false,
-	// 	muted: true,
-	// 	floatingWhenNotVisible: false,
-	// 	posterOptions: {
-	// 		transformation: {
-	// 			startOffset: '0'
-	// 		}
-	// 	},
-	// 	autoplayMode: 'on-scroll',
-	// 	autoplay: true
-	// });
-	//
-	// player.source('nest/c5g3xpzt4ubjuukiuxv5', {
-	// 	transformation: {
-	// 		fetch_format: 'auto'
-	// 	}
-	// });
-	const vid = new CloudinaryVideo('nest/c5g3xpzt4ubjuukiuxv5', {cloudName: 'dpiuthi6q'});
-	const videoEl = useRef(null);
-	const sources = [
-		{
-			type: 'mp4',
-			codecs: ['vp8', 'vorbis'],
-			transcode: videoCodec(autoVideo())
-		},
-		{
-			type: 'webm',
-			codecs: ['avc1.4D401E', 'mp4a.40.2'],
-			transcode: videoCodec(autoVideo())
-		}];
-
-	return (
-		<div>
-			<AdvancedVideo cldVid={vid} sources={sources} ref={videoEl} controls />
-<AdvancedVideo cldVid={myVideo} />
-
-		</div>
-	)
-};
-export default CloudinaryVideoCustom
+// 'use client'
+// import { useRef } from "react";
+// import player from "cloudinary-video-player";
+// import { AdvancedVideo } from "@cloudinary/react";
+// import { videoCodec } from "@cloudinary/url-gen/actions/transcode";
+// import { auto } from "@cloudinary/url-gen/qualifiers/videoCodec";
+// import { CloudinaryVideo } from "@cloudinary/url-gen";
+// import "cloudinary-video-player/dist/cld-video-player.min.css";
+// // @ts-ignore
+// const VideoPlayer = (props) => {
+// 	const vid = new CloudinaryVideo('dog', {cloudName: 'demo'});
+// 	const videoEl = useRef();
+// 	// @ts-ignore
+// 	const player = vid.videoPlayer('player', {
+// 		fluid: true,
+// 		controls: true
+// 	});
+//
+// 	player.source('nest/c5g3xpzt4ubjuukiuxv5');
+// // Modify player source and play hls adaptive streaming
+// 	player.source('rafting', { sourceTypes: ['hls'],
+// 		transformation: { streaming_profile: 'full_hd' } }).play();
+// 	const sources = [
+// 		{
+// 			type: 'mp4',
+// 			codecs: ['vp8', 'vorbis'],
+// 			transcode: videoCodec(auto())
+// 		},
+// 		{
+// 			type: 'webm',
+// 			codecs: ['avc1.4D401E', 'mp4a.40.2'],
+// 			videoCodec: videoCodec(auto())
+// 		}];
+//
+//
+// 	return (
+// 		<div>
+// 			<AdvancedVideo
+// 				// @ts-ignore
+// 				cldVid={vid} sources={sources} ref={videoEl} controls />
+//
+// 			<video src={player}/>
+// 		</div>
+// 	)
+// };
+// export default VideoPlayer;
--- components/VideoPlayer/MemoVid.tsx
+++ components/VideoPlayer/MemoVid.tsx
@@ -1,20 +1,53 @@
-import { forwardRef, memo } from "react";
-
-const MemoVidPlayer = memo( forwardRef(function  ({ auto, inView, handleProgress, src, poster }:any, ref) { {
-	return (
-		<video
-			muted={auto}
-			playsInline={true}
-			autoPlay={auto}
-			style={{ width: "100%", height: "100%", objectFit: "cover"}}
-			onTimeUpdate={handleProgress}
-			preload="none"
-			// @ts-ignore
-
-		 	ref={ref}
-			src={`https://www.nestrecovery.me${src}`}>
-			<p>Sorry, your browser does&quote;t support embedded video.</p>
-		</video>
-	);
-}}));
-export default MemoVidPlayer
+// 'use client'
+// import { Video, CloudinaryContext } from "cloudinary-react";
+// import { useState, useEffect, memo } from "react";
+// import { useInView } from "react-intersection-observer";
+// import { AdvancedVideo } from "@cloudinary/react";
+// import { CloudinaryVideo } from "@cloudinary/url-gen";
+// import { videoCodec } from "@cloudinary/url-gen/actions/transcode";
+// import { auto } from "@cloudinary/url-gen/qualifiers/videoCodec";
+// import { format } from "@cloudinary/url-gen/actions/delivery";
+//
+// // @ts-ignore
+// // eslint-disable-next-line react/display-name
+// export const MemoVidPlayer = memo(({ publicId }) => {
+// 	// @ts-ignore
+// 	let vid =  new CloudinaryVideo('nest/xjpchft1uit3cf5gzhtt', {cloudName: 'dpiuthi6q'})
+//
+// 	return (
+// 		<CloudinaryContext cloudName="dpiuthi6q">
+// 			<AdvancedVideo cldVid={vid} sources={ [
+// 			{
+// 				type: 'mp4',
+// 				codecs: ['vp8', 'vorbis'],
+// 				transcode: videoCodec(auto())
+// 			},
+// 			{
+// 				type: 'webm',
+// 				codecs: ['avc1.4D401E', 'mp4a.40.2'],
+// 				transcode: videoCodec(auto())
+// 			}]} controls   autoPlay muted />
+// 		</CloudinaryContext>
+// 	);
+// });
+//
+// export const VideoPlayer = ({ vidPublicId = "video-blog/cat" }) => {
+//
+// 	const [publicId, setPublicId] = useState("");
+// 	const { ref, inView } = useInView({ threshold: 1 });
+//
+// 	useEffect(() => {
+// 		if (inView) {
+// 			setPublicId(vidPublicId);
+// 		}
+// 	}, [inView, vidPublicId]);
+//
+//
+// 	return (
+// 		<div ref={ref}>
+// 			<MemoVidPlayer
+// 				// @ts-ignore
+// 				publicId={publicId} />
+// 		</div>
+// 	);
+// };
