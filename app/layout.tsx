7c856ef10d5a1ef0b2a995769f2e6b113e6016fc
DmitriyAgapov <prweb2012@gmail.com>
2023-11-09 09:48:42 +0500

    inline script, clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { font } from "@/assets/fonts/fonts";
 import Layout from "@/components/layout/layout";
 import { Suspense } from "react";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -24,12 +23,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-    <Script strategy={"afterInteractive"} id={'clarity'} type="text/javascript" dangerouslySetInnerHTML={{ __html:`
-      (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "jnlf1ol7be");`}}/>
+
+
+
       <body className={font.className}>
 
 
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -12,8 +12,16 @@ const Layout = ({
 	const envDev = process.env.NODE_ENV !== "development"
 	return (
 		<>
-			{envDev && (<><Script src="https://www.googletagmanager.com/gtag/js?id=G-X7K6VG59EG" />
-
+			{envDev && (<>
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-X7K6VG59EG" />
+			<Script strategy={"afterInteractive"} id={'clarity'}
+					dangerouslySetInnerHTML={{ __html:`
+					  (function(c,l,a,r,i,t,y){
+					c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+					t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+					y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+				})(window, document, "clarity", "script", "jnlf1ol7be");`}}
+			/>
 			<Script id="gtag-google" dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
